bbb313a7fb8b388f29ca4daaceccf72a9522b0d8
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-12 18:31:25 -0400

    Update sum_of_divisors.cpp

--- sum_of_divisors.cpp
+++ sum_of_divisors.cpp
@@ -5,10 +5,10 @@ int sumofdivisors(int number)
 {
     //Accumulate all the answers
     int sum=0;
-    for (int i=1; i<=number; i++){
+    for (int i=1; i<=number; i++)
+    {
          if(number%i==0)
             sum+=i;
-
     }
 return sum;
 }
