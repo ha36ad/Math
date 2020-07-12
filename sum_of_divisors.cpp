d2cb7a05569cf83910fab15753ebfa71951a539d
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-12 18:27:35 -0400

    Create sum_of_divisors.cpp

---
+++ sum_of_divisors.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+// Function to calculate sum of divisors
+int sumofdivisors(int number)
+{
+    //Accumulate all the answers
+    int sum=0;
+    for (int i=1; i<=number; i++){
+         if(number%i==0)
+            sum+=i;
+
+    }
+return sum;
+}
+//Run the program
+int main()
+{
+    // change the number here
+   int number = 365;
+    cout << "Sum of divisors: " << sumofdivisors(number); 
+    return 0;
+
+}
