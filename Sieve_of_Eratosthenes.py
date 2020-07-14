6f0d8c442b7cfbd9500d4123008969724bc49f01
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-14 14:59:58 -0400

    Update Sieve_of_Eratosthenes.py

--- Sieve_of_Eratosthenes.py
+++ Sieve_of_Eratosthenes.py
@@ -1,3 +1,19 @@
 import math
+
 #Ask user for a number
 number =int(input("Enter an integer:"))
+
+#Creating an array
+prime_numbers = []
+
+#Storing all numbers 
+for i in range (2,number+1):
+    prime_numbers.append(i)
+#Deleting multiples
+j = 2
+while(j<=int(math.sqrt(number))):
+    for i in range (j*j,number+1,j):
+        if i in prime_numbers:
+            prime_numbers.remove(i)
+    j = j+1
+print(prime_numbers)
