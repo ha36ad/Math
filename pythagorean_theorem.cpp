309813df644862a1479b9ef03e9aee2e7e687e5e
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-17 15:19:08 -0400

    Create pythagorean_theorem.cpp

---
+++ pythagorean_theorem.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+float a;
+float b;
+float c;
+//Ask user for inputs
+	std::cout << "Enter a value for a: ";
+	std::cin >> a;
+
+	std::cout << "Enter a value for b: ";
+	std::cin >> b;
+//Calculations
+a = pow(a,2);
+b = pow(b,2);
+c = sqrt(a+b);
+
+//Print c
+std::cout << "C: " << c << std::endl;
+
+return 0;
+}
