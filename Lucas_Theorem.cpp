38ffd8538611bd0fb3b1507b11402f0c070e735d
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-22 22:18:37 -0400

    Create Lucas_Theorem.cpp

---
+++ Lucas_Theorem.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Main Function
+int LucasnCr (number,r,p)
+{
+
+}
+
+//Running the program
+int main()
+{
+  int number = 1000;
+  int r = 500;
+  int p = 15;
+  cout << "NcR is " << LucasnCr (n,r,p);
+  return 0;
+}
