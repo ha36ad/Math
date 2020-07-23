11671a6c5bf7863b3971f0a66ddd93cff307f7a4
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-23 15:15:28 -0400

    Update Lucas_Theorem.cpp

--- Lucas_Theorem.cpp
+++ Lucas_Theorem.cpp
@@ -2,17 +2,39 @@
 using namespace std;
 
 //Main Function
-int LucasnCr (number,r,p)
+int LucasnCr (int number, int r, int p)
 {
-
+//Pascal's triangle
+int C [r+1];
+C[r+1] = 0;
+C[0] = 1;
+for (int i =1; i <= number; i++)
+{
+    for (int j = 0; j <= i; i++)
+    {
+        C[j] = C[j]+ C[j-1] %p;
+    }
 }
+//Main function
+int LucasnCrmain (int n, int r, int p)
+{
+    //Base case
+    if (r==0)
+    {
+        return 1;
+    }
 
+    int n = number%p;
+    int ri = r%p;
+    return (LucasnCrmain(number/p, r/p,p) * (LucasnCr(n,ri,p))%p);
+}
+  
 //Running the program
 int main()
 {
   int number = 1000;
-  int r = 500;
-  int p = 15;
-  cout << "NcR is " << LucasnCr (n,r,p);
+  int r = 300;
+  int p = 13;
+  cout << "NcR is " << LucasnCrmain (n,r,p);
   return 0;
 }
