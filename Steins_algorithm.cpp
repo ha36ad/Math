2a1cdc378c9f580bfe4d6291b9af1019b4d39f75
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-28 19:25:26 -0400

    Update Steins_algorithm.cpp

--- Steins_algorithm.cpp
+++ Steins_algorithm.cpp
@@ -17,12 +17,37 @@ int Stein(int u, int v)
     {
         return u;
     }
+     //Check for factors of 2
+     if (~u & 1) //  if u even
+     {
+          if (v & 1) // if v odd
+          {
+               return Stein(u >> 1, v);
+          }
+          else //both are even
+            {
+                 return Stein(u >> 1, v >> 1) << 1;
+            }
+     }
+     if (~v &1) // u is odd and v is even
+     {
+          return Stein(u, v >> 1);
+     }
+   //Reducing larger cases
+       if (u > v)
+       {
+           return Stein((u - v) >> 1, v);
+       }
+
+    return Stein((v - u) >> 1, u);
 }
+
 
 //Run the program
 int main()
 {
-    int u = 25;
-    int v = 19;
-    printf("The GCD is: " ,Stein(u,v));
+    int u = 34;
+    int v = 24;
+    cout <<"The GCD is: " << Stein(u,v);
+    return 0;
 }
