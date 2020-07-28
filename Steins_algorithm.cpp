7d6f829869cc1ea79f94c31efbe36102b6edd87a
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-27 22:04:01 -0400

    Create Steins_algorithm.cpp

---
+++ Steins_algorithm.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Algorithm function
+int Stein(int u, int v){
+    
+}
+
+//Run the program
+int main()
+{
+    int u = 25;
+    int v = 19;
+    printf("The GDC is: " ,Stein(u,v));
+}
