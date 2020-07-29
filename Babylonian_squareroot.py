220b4e5081e5d6698e3500060f2ea17723db0790
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-29 15:32:43 -0400

    Create Babylonian_squareroot.py

---
+++ Babylonian_squareroot.py
@@ -0,0 +1,25 @@
+#Ask user for a number
+number = float(input("Enter a number: "))
+#Controls precision
+epilson = 0.00001
+
+def Square_root(number):  
+    #check if number is 0 
+    if (number == 0):
+     print("The square root is 0")
+    #check if number is negative
+    elif (number < 0):
+        print("Input positive values only")
+    #Ask user for a guess
+    guess =  float(input("What is your initial guess?: "))
+
+    #Main algorithm
+    x = guess
+    y= 1
+    while (x-y >= epilson):
+     x = (x +number/x)/2.0
+     y = number/x
+    return x 
+
+#Print square root
+print(round(Square_root(number),6))
