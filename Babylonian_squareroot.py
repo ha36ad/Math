6ca50a7ba20132c5d09835ee7b50409c5b25873a
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-29 16:40:30 -0400

    Update Babylonian_squareroot.py

--- Babylonian_squareroot.py
+++ Babylonian_squareroot.py
@@ -7,6 +7,9 @@ def Square_root(number):
     #check if number is 0 
     if (number == 0):
      print("The square root is 0")
+    #check if number is 1
+    if (number ==1):
+        print("The square root is 1")
     #check if number is negative
     elif (number < 0):
         print("Input positive values only")
@@ -22,4 +25,4 @@ def Square_root(number):
     return x 
 
 #Print square root
-print(round(Square_root(number),7))
+print("The square root of ", number, " is ", (round(Square_root(number),7))
