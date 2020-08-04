fe1d9b10e1f34fcb888fb362701674ca71d9fc3e
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-08-03 22:54:49 -0400

    Create Sphenic_number.cpp

---
+++ Sphenic_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Max limit for factors
+const int LIMIT = 1000;
+
+void PrimeFactors(int number)
+{
+}
+
+bool Sphenic(int number)
+{
+}
+
+int main()
+{
+}
