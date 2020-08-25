e8e95748ae8b1d858ac6d21435062829f7ba2b59
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-08-24 22:11:39 -0400

    Update names_scores.py

--- Project_Euler/names_scores.py
+++ Project_Euler/names_scores.py
@@ -1,5 +1,10 @@
+from string import ascii_uppercase
 
+def score(name):
+    result = list(ascii_uppercase)
+    return sum(result.index(i) + 1 for i in name.strip('"'))
 
 with open('euler_22.txt') as f:
   names = f.read().split(',')
   names.sort()
+ print (sum( i* score(j) for i, j in enumerate(names,1)))
